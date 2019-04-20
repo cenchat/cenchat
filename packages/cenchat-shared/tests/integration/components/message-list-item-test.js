f1a3a43329e1aa8fe9da2a3ce49f85be1ac3134d
Mikko Paderes <rmmpaderes@gmail.com>
2019-04-20 22:48:21 +0800

    Add chip badge for chat creator

--- packages/cenchat-shared/tests/integration/components/message-list-item-test.js
+++ packages/cenchat-shared/tests/integration/components/message-list-item-test.js
@@ -1,9 +1,10 @@
+import { A } from '@ember/array';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-import { setupTestState } from '@cenchat/shared/test-support';
+import { setupTestState, timeout } from '@cenchat/shared/test-support';
 
 module('Integration | Component | message-list-item', function (hooks) {
   setupRenderingTest(hooks);
@@ -13,12 +14,12 @@ module('Integration | Component | message-list-item', function (hooks) {
 
     const store = this.owner.lookup('service:store');
     const messageA = await store.findRecord('message', 'message_a');
-    const messageB = await store.findRecord('message', 'message_a');
+    const messageB = await store.findRecord('message', 'message_b');
 
     this.set('messageGroup', {
       author: messageA.get('author'),
       timestamp: messageA.get('createdOn'),
-      messages: [messageA, messageB],
+      messages: new A([messageA, messageB]),
     });
   });
 
@@ -32,5 +33,33 @@ module('Integration | Component | message-list-item', function (hooks) {
     assert.dom('[data-test-message-list-item="author-name"]').hasText('User B');
     assert.dom('[data-test-message-list-item="timestamp"]').exists();
     assert.dom('[data-test-message-list-item="content"]').exists({ count: 2 });
+  });
+
+  test('should mark author name as badged when author is also the chat creator', async function (assert) {
+    assert.expect(1);
+
+    // Act
+    await render(hbs`<MessageListItem @messageGroup={{this.messageGroup}} />`);
+
+    // Assert
+    await timeout(100); // Wait for relationships to load
+    assert.dom('[data-test-message-list-item="author-name"]').hasClass('message-list-item__author-name--badged');
+  });
+
+  test('should not mark author name as badged when author is not the chat creator', async function (assert) {
+    assert.expect(1);
+
+    // Arrange
+    const store = this.owner.lookup('service:store');
+    const author = await store.findRecord('user', 'user_a');
+
+    this.set('messageGroup.author', author);
+
+    // Act
+    await render(hbs`<MessageListItem @messageGroup={{this.messageGroup}} />`);
+
+    // Assert
+    await timeout(100); // Wait for relationships to load
+    assert.dom('[data-test-message-list-item="author-name"]').doesNotHaveClass('message-list-item__author-name--badged');
   });
 });
