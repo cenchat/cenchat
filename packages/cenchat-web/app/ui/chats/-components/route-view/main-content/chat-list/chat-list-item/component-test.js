0c562226e070cc63c0fa0d0c24ceb878bcece5e3
Mikko Paderes <rmmpaderes@gmail.com>
2019-05-26 14:30:52 +0800

    Improve chat list UI (#41)

--- packages/cenchat-web/app/ui/chats/-components/route-view/main-content/chat-list/chat-list-item/component-test.js
+++ packages/cenchat-web/app/ui/chats/-components/route-view/main-content/chat-list/chat-list-item/component-test.js
@@ -17,8 +17,8 @@ module('Integration | Component | chats/-components/route-view/main-content/chat
     this.set('chat', chat);
   });
 
-  test('should show chat info', async function (assert) {
-    assert.expect(3);
+  test('should show chat info for site member', async function (assert) {
+    assert.expect(4);
 
     // Arrange
     await setupAuthState({
@@ -30,7 +30,27 @@ module('Integration | Component | chats/-components/route-view/main-content/chat
 
     // Assert
     await timeout(100); // Wait for relationship to load and render
-    assert.dom('[data-test-chat-list-item="name"]').hasText('Site A');
+    assert.dom('[data-test-chat-list-item="creator-username-for-site-member"]').hasText('user_b');
+    assert.dom('[data-test-chat-list-item="page-info-for-site-member"]').hasText('Site A - Page A Title');
+    assert.dom('[data-test-chat-list-item="message"]').hasText('user_b: Message B');
+    assert.dom('[data-test-chat-list-item="timestamp"]').exists();
+  });
+
+  test('should show chat info for chat creator', async function (assert) {
+    assert.expect(4);
+
+    // Arrange
+    await setupAuthState({
+      user: { uid: 'user_b' },
+    });
+
+    // Act
+    await render(hbs`{{chats/-components/route-view/main-content/chat-list/chat-list-item chat=this.chat}}`);
+
+    // Assert
+    await timeout(100); // Wait for relationship to load and render
+    assert.dom('[data-test-chat-list-item="site-name-for-creator"]').hasText('Site A');
+    assert.dom('[data-test-chat-list-item="page-title-for-creator"]').hasText('Page A Title');
     assert.dom('[data-test-chat-list-item="message"]').hasText('user_b: Message B');
     assert.dom('[data-test-chat-list-item="timestamp"]').exists();
   });
