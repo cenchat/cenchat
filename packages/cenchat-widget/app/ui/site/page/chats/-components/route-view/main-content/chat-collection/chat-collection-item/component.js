66fb671ad87d45eec491f7aa8f0366f8ad4b5243
Mikko Paderes <rmmpaderes@gmail.com>
2019-08-04 13:02:20 +0800

    Fix read more not appearing when needed

--- packages/cenchat-widget/app/ui/site/page/chats/-components/route-view/main-content/chat-collection/chat-collection-item/component.js
+++ packages/cenchat-widget/app/ui/site/page/chats/-components/route-view/main-content/chat-collection/chat-collection-item/component.js
@@ -29,9 +29,10 @@ export default Component.extend({
   async setupIsReadMoreVisible() {
     const first3Messages = await this.args.chat.get('first3Messages');
     const texts = first3Messages.map(message => message.get('text'));
-    const mergedText = texts.join('\n');
+    const mergedText = texts.join();
+    const numOfNewLines = mergedText.split('\n').length;
 
-    if (mergedText.length > 180) {
+    if (mergedText.length > 180 || (mergedText.length > 100 && numOfNewLines > 2)) {
       this.set('isReadMoreVisible', true);
     }
   },
